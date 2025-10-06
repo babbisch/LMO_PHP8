e3acff8eb8049702f35ea7b8189a26d94b7f16ef
babbisch <dummwiebrot@gmail.com>
2025-10-06 19:10:42 +0200

    Update lmo-showcal.php
    remove some divs and highlight weekend

--- lmo/lmo-showcal.php
+++ lmo/lmo-showcal.php
@@ -1,27 +1,19 @@
-<div class="container-fluid"><?php if (strlen($cal) > 4) { ?>
+<div class="container"><?php if (strlen($cal) > 4) { ?>
   <div class="row justify-content-center">
-    <div class="col">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-3 text-end"><a href="<?php echo $addk . $ma; ?>" title="<?php echo $text[142]; ?>"><?php echo $text[5]; ?></a></div>
-          <div class="col-6 text-center"><a href="<?php echo $addk . $ml; ?>" title="<?php echo $text[173]; ?>"><?php echo $md; ?></a></div>
-          <div class="col-3 text-start"><a href="<?php echo $addk . $mb; ?>" title="<?php echo $text[143]; ?>"><?php echo $text[154]; ?></a>&nbsp;&nbsp;<a href="<?php echo $addk . $mc; ?>" title="<?php echo $text[144]; ?>"><?php echo $text[7]; ?></a></div>
-        </div>
-      </div>
-    </div>
+    <div class="col-3 text-end"><a href="<?php echo $addk . $ma; ?>" title="<?php echo $text[142]; ?>"><?php echo $text[5]; ?></a></div>
+    <div class="col-6 text-center"><a href="<?php echo $addk . $ml; ?>" title="<?php echo $text[173]; ?>"><?php echo $md; ?></a></div>
+    <div class="col-3 text-start"><a href="<?php echo $addk . $mb; ?>" title="<?php echo $text[143]; ?>"><?php echo $text[154]; ?></a>&nbsp;&nbsp;<a href="<?php echo $addk . $mc; ?>" title="<?php echo $text[144]; ?>"><?php echo $text[7]; ?></a></div>
   </div>
   <div class="row justify-content-center">
-    <div class="col">
-      <div class="container-fluid">
-        <div class="row justify-content-center">
-          <div class="col-1"><strong><?php echo $text[147]; ?></strong></div>
-          <div class="col-1"><strong><?php echo $text[148]; ?></strong></div>
-          <div class="col-1"><strong><?php echo $text[149]; ?></strong></div>
-          <div class="col-1"><strong><?php echo $text[150]; ?></strong></div>
-          <div class="col-1"><strong><?php echo $text[151]; ?></strong></div>
-          <div class="col-1"><strong><?php echo $text[152]; ?></strong></div>
-          <div class="col-1"><strong><?php echo $text[153]; ?></strong></div>
-        </tr>
+    <div class="row justify-content-center">
+      <div class="col-1"><strong><?php echo $text[147]; ?></strong></div>
+      <div class="col-1"><strong><?php echo $text[148]; ?></strong></div>
+      <div class="col-1"><strong><?php echo $text[149]; ?></strong></div>
+      <div class="col-1"><strong><?php echo $text[150]; ?></strong></div>
+      <div class="col-1"><strong><?php echo $text[151]; ?></strong></div>
+      <div class="col-1"><strong><?php echo $text[152]; ?></strong></div>
+      <div class="col-1"><strong><?php echo $text[153]; ?></strong></div>
+   </div>
 <?php
     $dat0 = getdate(time());
     if ($erster != 1) {
@@ -45,13 +37,13 @@
             $dum7 = $dat4['mday'] . '.' . $dat4['mon'] . '.' . $dat4['year'];
             if ($dum6 == $dum7) {
                 if (($heute == 6) || ($heute == 7)) {
-                    $stil = 'bg-warning';
+                    $stil = 'bg-secondary-subtle';
                 } else {
                     $stil = 'bg-info';
                 }
             } else {
                 if (($heute == 6) || ($heute == 7)) {
-                    $stil = 'bg-warning';
+                    $stil = 'bg-secondary-subtle';
                 } else {
                     $stil = '';
                 }
@@ -85,19 +77,10 @@
 } else {
 ?>
   <div class="row justify-content-center">
-    <div class="col">
-      <div class="container-fluid">
-        <div class='row'>
-          <div class="col-3 text-end"><a href="<?php echo $addk . $ma; ?>" title="<?php echo $text[157]; ?>"><?php echo $text[5]; ?></a></div>
-          <div class="col-6 text-center"><?php echo $text[156] . ' ' . $md; ?></div>
-          <div class="col-3 text-start"><a href="<?php echo $addk . $mb; ?>" title="<?php echo $text[158]; ?>"><?php echo $text[154]; ?></a>&nbsp;&nbsp;<a href="<?php echo $addk . $mc; ?>" title="<?php echo $text[159]; ?>"><?php echo $text[7]; ?></a></div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col justify-content-center">
-      <tdiv class="container-fluid"><?php
+    <div class="col-3 text-end"><a href="<?php echo $addk . $ma; ?>" title="<?php echo $text[157]; ?>"><?php echo $text[5]; ?></a></div>
+    <div class="col-6 text-center"><?php echo $text[156] . ' ' . $md; ?></div>
+    <div class="col-3 text-start"><a href="<?php echo $addk . $mb; ?>" title="<?php echo $text[158]; ?>"><?php echo $text[154]; ?></a>&nbsp;&nbsp;<a href="<?php echo $addk . $mc; ?>" title="<?php echo $text[159]; ?>"><?php echo $text[7]; ?></a></div>
+  </div><?php
     $dat0 = getdate(time());
     $lmo_output_buffer = array();
     for ($i = 1; $i <= 12; $i++) {
@@ -124,7 +107,5 @@
     }
 }
 ?>
-      </div>
-    </div>
   </div>
 </div>
